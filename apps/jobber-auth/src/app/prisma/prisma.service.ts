6198386f0768aae61ef16317f93f4e9ea6c31f16
API-Imperfect <alphaogilo@gmail.com>
2026-02-09 16:56:52 +0200

    create and get users

--- apps/jobber-auth/src/app/prisma/prisma.service.ts
+++ apps/jobber-auth/src/app/prisma/prisma.service.ts
@@ -1,8 +1,18 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma-clients/jobber-auth';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const connectionString = process.env.AUTH_DATABASE_URL;
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
---
+++ apps/jobber-auth/src/app/users/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from './models/user.model';
+import { Prisma } from '@prisma-clients/jobber-auth';
+import { hash } from 'bcryptjs';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prismaService.user.create({
+      data: {
+        ...data,
+        password: await hash(data.password, 10),
+      },
+    });
+  }
+
+  async getUsers(): Promise<User[]> {
+    return this.prismaService.user.findMany();
+  }
+}
